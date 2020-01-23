66f565cda6b3a656c4ef7c6f95fd9bf415c69770
Yannick Knapp <YaBaSa@web.de>
2020-01-23 09:05:37 +0000

    Change weight

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python eval_mAP.py --model_def config/complex_yolov3_low_res.cfg --weights_path checkpoints/yolov3_ckpt_epoch-290_MAP-0.82.pth
+python eval_mAP.py --model_def config/complex_yolov3_low_res.cfg --weights_path checkpoints/test_0/yolov3_ckpt_epoch-36_MAP-0.72.pth

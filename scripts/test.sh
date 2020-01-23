305507fdbbf090424da621dd381a3b0dc7655f53
Yannick Knapp <YaBaSa@web.de>
2020-01-23 08:14:52 +0000

    Adapt scripts to current weight

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python test_detection.py --model_def config/complex_yolov3_low_res.cfg --weights_path checkpoints/yolov3_ckpt_epoch-290_MAP-0.82.pth
+python test_detection.py --model_def config/complex_yolov3_low_res.cfg --weights_path checkpoints/test_0/yolov3_ckpt_epoch-36_MAP-0.72.pth

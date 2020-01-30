19e3ac0334c7587bd15f00818b4505c1cb0a25ea
Yannick Knapp <YaBaSa@web.de>
2020-01-30 12:32:20 +0000

    Add subfolder 'weights'

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python test_detection.py --model_def config/complex_yolov3_low_res.cfg --weights_path checkpoints/test_1/yolov3_ckpt_epoch-290_MAP-0.82.pth
+python test_detection.py --model_def config/complex_yolov3_low_res.cfg --weights_path checkpoints/test_1/weights/yolov3_ckpt_epoch-290_MAP-0.82.pth

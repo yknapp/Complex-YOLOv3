4915660b8f32965e972d391951117fc8b3a3946b
Yannick Knapp <YaBaSa@web.de>
2020-02-18 12:52:28 +0000

    Add launching parameters

--- perform_bev_transformation.py
+++ perform_bev_transformation.py
@@ -1,15 +1,15 @@
 import os
+import argparse
 import numpy as np
 import utils.config as cnf
 import utils.dataset_bev_utils as bev_utils
 
 from unit.lyft2kitti_converter import Lyft2KittiConverter
 
-UNIT_CONFIG_PATH = '/home/user/work/master_thesis/code/UNIT/outputs/unit_bev_lyft2kitti_2channel_folder_8/config.yaml'
-UNIT_CHECKPOINT_PATH = '/home/user/work/master_thesis/code/UNIT/outputs/unit_bev_lyft2kitti_2channel_folder_8/checkpoints/gen_00010000.pt'
 CHOSEN_EVAL_FILES_PATH = 'data/LYFT/ImageSets/valid.txt'
 BEV_OUTPUT_PATH = '/home/user/work/master_thesis/datasets/lyft_kitti/object/training/bev'
 LYFT_LIDAR_PATH = '/home/user/work/master_thesis/datasets/lyft_kitti/object/training/velodyne'
+
 
 def get_lidar_lyft(idx):
     lidar_file = os.path.join(LYFT_LIDAR_PATH, '%s.bin' % idx)
@@ -28,8 +28,16 @@ def perform_img2img_translation(lyft2kitti_conv, np_img):
     np_img_output[1, :, :] = np_img_transformed[1, :, :]
     return np_img_output
 
+
 def main():
-    lyft2kitti_conv = Lyft2KittiConverter(UNIT_CONFIG_PATH, UNIT_CHECKPOINT_PATH)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_def", type=str, default="config/complex_tiny_yolov3.cfg", help="path to model definition file")
+    parser.add_argument("--weights_path", type=str, default="checkpoints/tiny-yolov3_ckpt_epoch-220.pth", help="path to weights file")
+    parser.add_argument('--unit_config', type=str, default=None, help="UNIT net configuration")
+    parser.add_argument('--unit_checkpoint', type=str, default=None, help="checkpoint of UNIT autoencoders")
+    opt = parser.parse_args()
+
+    lyft2kitti_conv = Lyft2KittiConverter(opt.unit_config, opt.unit_checkpoint)
 
     # get validation images which are chosen for evaluation
     image_filename_list = [x.strip() for x in open(CHOSEN_EVAL_FILES_PATH).readlines()]
@@ -46,6 +54,7 @@ def main():
         bev_array_transformed = perform_img2img_translation(lyft2kitti_conv, bev_array)
         output_path = os.path.join(BEV_OUTPUT_PATH, image_filename)
         np.save('%s.npy' % output_path, bev_array_transformed)
+
 
 if __name__ == '__main__':
     main()

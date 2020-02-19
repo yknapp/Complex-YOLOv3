8a30b7a0ce1a66a26d93895d5d1827ccdc3bb521
Yannick Knapp <YaBaSa@web.de>
2020-02-19 09:18:08 +0000

    Create parent class 'Calibration' which 'KittiCalibration' inherits

--- utils/calibration.py
+++ utils/calibration.py
@@ -1,77 +1,10 @@
 import numpy as np
 import utils.dataset_utils as dataset_utils
 
-class KittiCalibration(object):
-    ''' Calibration matrices and utils
-        3d XYZ in <label>.txt are in rect camera coord.
-        2d box xy are in image2 coord
-        Points in <lidar>.bin are in Velodyne coord.
 
-        y_image2 = P^2_rect * x_rect
-        y_image2 = P^2_rect * R0_rect * Tr_velo_to_cam * x_velo
-        x_ref = Tr_velo_to_cam * x_velo
-        x_rect = R0_rect * x_ref
-
-        P^2_rect = [f^2_u,  0,      c^2_u,  -f^2_u b^2_x;
-                    0,      f^2_v,  c^2_v,  -f^2_v b^2_y;
-                    0,      0,      1,      0]
-                 = K * [1|t]
-
-        image2 coord:
-         ----> x-axis (u)
-        |
-        |
-        v y-axis (v)
-
-        velodyne coord:
-        front x, left y, up z
-
-        rect/ref camera coord:
-        right x, down y, front z
-
-        Ref (KITTI paper): http://www.cvlibs.net/publications/Geiger2013IJRR.pdf
-
-        TODO(rqi): do matrix multiplication only once for each projection.
-    '''
-
-    def __init__(self, calib_filepath):
-        calibs = self.read_calib_file(calib_filepath)
-        # Projection matrix from rect camera coord to image2 coord
-        self.P = calibs['P2']
-        self.P = np.reshape(self.P, [3, 4])
-        # Rigid transform from Velodyne coord to reference camera coord
-        self.V2C = calibs['Tr_velo2cam']
-        self.V2C = np.reshape(self.V2C, [3, 4])
-        self.C2V = dataset_utils.inverse_rigid_trans(self.V2C)
-        # Rotation from reference camera coord to rect camera coord
-        self.R0 = calibs['R_rect']
-        self.R0 = np.reshape(self.R0, [3, 3])
-
-        # Camera intrinsics and extrinsics
-        self.c_u = self.P[0, 2]
-        self.c_v = self.P[1, 2]
-        self.f_u = self.P[0, 0]
-        self.f_v = self.P[1, 1]
-        self.b_x = self.P[0, 3] / (-self.f_u)  # relative
-        self.b_y = self.P[1, 3] / (-self.f_v)
-
+class Calibration(object):
     def read_calib_file(self, filepath):
-        with open(filepath) as f:
-            lines = f.readlines()
-
-        obj = lines[2].strip().split(' ')[1:]
-        P2 = np.array(obj, dtype=np.float32)
-        obj = lines[3].strip().split(' ')[1:]
-        P3 = np.array(obj, dtype=np.float32)
-        obj = lines[4].strip().split(' ')[1:]
-        R0 = np.array(obj, dtype=np.float32)
-        obj = lines[5].strip().split(' ')[1:]
-        Tr_velo_to_cam = np.array(obj, dtype=np.float32)
-
-        return {'P2': P2.reshape(3, 4),
-                'P3': P3.reshape(3, 4),
-                'R_rect': R0.reshape(3, 3),
-                'Tr_velo2cam': Tr_velo_to_cam.reshape(3, 4)}
+        pass
 
     def cart2hom(self, pts_3d):
         """
@@ -171,3 +104,77 @@ class KittiCalibration(object):
         boxes_corner = np.concatenate((x.reshape(-1, 8, 1), y.reshape(-1, 8, 1)), axis=2)
 
         return boxes, boxes_corner
+
+
+
+class KittiCalibration(Calibration):
+    ''' Calibration matrices and utils
+        3d XYZ in <label>.txt are in rect camera coord.
+        2d box xy are in image2 coord
+        Points in <lidar>.bin are in Velodyne coord.
+
+        y_image2 = P^2_rect * x_rect
+        y_image2 = P^2_rect * R0_rect * Tr_velo_to_cam * x_velo
+        x_ref = Tr_velo_to_cam * x_velo
+        x_rect = R0_rect * x_ref
+
+        P^2_rect = [f^2_u,  0,      c^2_u,  -f^2_u b^2_x;
+                    0,      f^2_v,  c^2_v,  -f^2_v b^2_y;
+                    0,      0,      1,      0]
+                 = K * [1|t]
+
+        image2 coord:
+         ----> x-axis (u)
+        |
+        |
+        v y-axis (v)
+
+        velodyne coord:
+        front x, left y, up z
+
+        rect/ref camera coord:
+        right x, down y, front z
+
+        Ref (KITTI paper): http://www.cvlibs.net/publications/Geiger2013IJRR.pdf
+
+        TODO(rqi): do matrix multiplication only once for each projection.
+    '''
+
+    def __init__(self, calib_filepath):
+        calibs = self.read_calib_file(calib_filepath)
+        # Projection matrix from rect camera coord to image2 coord
+        self.P = calibs['P2']
+        self.P = np.reshape(self.P, [3, 4])
+        # Rigid transform from Velodyne coord to reference camera coord
+        self.V2C = calibs['Tr_velo2cam']
+        self.V2C = np.reshape(self.V2C, [3, 4])
+        self.C2V = dataset_utils.inverse_rigid_trans(self.V2C)
+        # Rotation from reference camera coord to rect camera coord
+        self.R0 = calibs['R_rect']
+        self.R0 = np.reshape(self.R0, [3, 3])
+
+        # Camera intrinsics and extrinsics
+        self.c_u = self.P[0, 2]
+        self.c_v = self.P[1, 2]
+        self.f_u = self.P[0, 0]
+        self.f_v = self.P[1, 1]
+        self.b_x = self.P[0, 3] / (-self.f_u)  # relative
+        self.b_y = self.P[1, 3] / (-self.f_v)
+
+    def read_calib_file(self, filepath):
+        with open(filepath) as f:
+            lines = f.readlines()
+
+        obj = lines[2].strip().split(' ')[1:]
+        P2 = np.array(obj, dtype=np.float32)
+        obj = lines[3].strip().split(' ')[1:]
+        P3 = np.array(obj, dtype=np.float32)
+        obj = lines[4].strip().split(' ')[1:]
+        R0 = np.array(obj, dtype=np.float32)
+        obj = lines[5].strip().split(' ')[1:]
+        Tr_velo_to_cam = np.array(obj, dtype=np.float32)
+
+        return {'P2': P2.reshape(3, 4),
+                'P3': P3.reshape(3, 4),
+                'R_rect': R0.reshape(3, 3),
+                'Tr_velo2cam': Tr_velo_to_cam.reshape(3, 4)}

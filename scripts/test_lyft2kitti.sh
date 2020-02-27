4efa28d984a21d24d25f540c379085d9ca657e5c
Yannick Knapp <YaBaSa@web.de>
2020-02-27 16:14:38 +0000

    Improve script structure

--- scripts/test_lyft2kitti.sh
+++ scripts/test_lyft2kitti.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python test_detection.py --dataset lyft2kitti --model_def config/complex_yolov3_low_res.cfg --weights_path checkpoints/test_1/weights/yolov3_ckpt_epoch-290_MAP-0.82.pth --unit_config /home/user/work/master_thesis/code/UNIT/outputs/unit_bev_lyft2kitti_2channel_folder_11/config.yaml --unit_checkpoint /home/user/work/master_thesis/code/UNIT/outputs/unit_bev_lyft2kitti_2channel_folder_11/checkpoints/gen_00015500.pt
+unit_model_folder="unit_bev_new_lyft2kitti_2channel_folder_2"
+unit_model_dir="/home/user/work/master_thesis/code/UNIT/outputs/$unit_model_folder"
+unit_checkpoint_dir=$unit_model_dir/checkpoints/gen_00018000.pt
+
+python test_detection.py --dataset lyft2kitti --model_def config/complex_yolov3_low_res.cfg --weights_path checkpoints/test_1/weights/yolov3_ckpt_epoch-290_MAP-0.82.pth --unit_config $unit_model_dir/config.yaml --unit_checkpoint $unit_checkpoint_dir

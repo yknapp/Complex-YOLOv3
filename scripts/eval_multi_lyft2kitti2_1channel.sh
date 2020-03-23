285d1cade323acdcca74581058ffd8152ff92cc2
Yannick Knapp <YaBaSa@web.de>
2020-03-23 08:29:38 +0000

    Update path to ComplexYOLO weight

--- scripts/eval_multi_lyft2kitti2_1channel.sh
+++ scripts/eval_multi_lyft2kitti2_1channel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-unit_model_folder="unit_bev_new_lyft2kitti_1channel_folder"
+unit_model_folder="unit_bev_new_lyft2kitti_2000_1channel_folder"
 unit_model_dir="/home/user/work/master_thesis/code/UNIT/outputs/$unit_model_folder"
 output_file="$unit_model_dir/$unit_model_folder.txt"
 "" > $output_file
@@ -12,9 +12,9 @@ do
   
   conda activate ComplexYOLO_0.4.1
 
-  python perform_bev_transformation.py --dataset lyft2kitti2 --model_def config/complex_yolov3_low_res.cfg --weights_path checkpoints/yolov3_ckpt_epoch-142_MAP-0.79.pth --unit_config $unit_model_dir/config.yaml --unit_checkpoint $checkpoint
+  python perform_bev_transformation.py --dataset lyft2kitti2 --model_def config/complex_yolov3_low_res.cfg --weights_path checkpoints/test_3/yolov3_ckpt_epoch-142_MAP-0.79.pth --unit_config $unit_model_dir/config.yaml --unit_checkpoint $checkpoint
 
   conda activate ComplexYOLO_1.1
   
-  python eval_mAP.py --dataset lyft2kitti2 --model_def config/complex_yolov3_low_res.cfg --weights_path checkpoints/yolov3_ckpt_epoch-142_MAP-0.79.pth >> $output_file
+  python eval_mAP.py --dataset lyft2kitti2 --model_def config/complex_yolov3_low_res.cfg --weights_path checkpoints/test_3/yolov3_ckpt_epoch-142_MAP-0.79.pth >> $output_file
 done

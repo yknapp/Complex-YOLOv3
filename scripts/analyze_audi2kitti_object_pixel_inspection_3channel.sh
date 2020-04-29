468f6d5fd0ecccba5bd002d8179a66f168c7d6a4
Yannick Knapp <YaBaSa@web.de>
2020-04-29 11:28:40 +0000

    Add audi2kitti object inspection analysis scripts

---
+++ scripts/analyze_audi2kitti_object_pixel_inspection_3channel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dataset=audi2kitti
+num_channel=3
+file_index=488
+
+unit_model_folder="unit_bev_new_audi2kitti_3channel_folder"
+unit_model_dir="/home/user/work/master_thesis/code/UNIT/outputs/$unit_model_folder"
+unit_checkpoint_dir=$unit_model_dir/checkpoints/gen_00037500.pt
+
+python analyze_object_pixel_inspection.py --file_index $file_index --dataset $dataset --num_channel $num_channel --unit_config $unit_model_dir/config.yaml --unit_checkpoint $unit_checkpoint_dir

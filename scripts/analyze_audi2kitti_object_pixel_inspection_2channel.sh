89c57510a092dfab1c8e255ac7e2698236704074
Yannick Knapp <YaBaSa@web.de>
2020-04-29 13:41:09 +0000

    Add more scripts for Audi object inspection

--- scripts/analyze_audi2kitti_object_pixel_inspection_2channel.sh
+++ scripts/analyze_audi2kitti_object_pixel_inspection_2channel.sh
@@ -2,10 +2,9 @@
 
 dataset=audi2kitti
 num_channel=2
-file_index=1
 
 unit_model_folder="unit_bev_new_audi2kitti_2channel_folder_3"
 unit_model_dir="/home/user/work/master_thesis/code/UNIT/outputs/$unit_model_folder"
 unit_checkpoint_dir=$unit_model_dir/checkpoints/gen_00037500.pt
 
-python analyze_object_pixel_inspection.py --file_index $file_index --dataset $dataset --num_channel $num_channel --unit_config $unit_model_dir/config.yaml --unit_checkpoint $unit_checkpoint_dir
+python analyze_mean_histogram.py --dataset $dataset --num_channel $num_channel --unit_config $unit_model_dir/config.yaml --unit_checkpoint $unit_checkpoint_dir
